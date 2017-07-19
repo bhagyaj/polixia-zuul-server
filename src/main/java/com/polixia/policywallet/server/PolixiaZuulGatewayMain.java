14546b0a326f376ab218619d29205cd328a85aa5
Bhagya Rupasinghe <brupasinghe@virtusapolaris.com>
2017-07-19 18:49:13 +0530

    polixia zuul server implemented

---
+++ src/main/java/com/polixia/policywallet/server/PolixiaZuulGatewayMain.java
@@ -0,0 +1,26 @@
+package com.polixia.policywallet.server;
+
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import javax.servlet.http.HttpServletRequest;
+/**
+ * Created by brupasinghe on 7/19/2017.
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PolixiaZuulGatewayMain {
+    private static Logger log = LoggerFactory.getLogger(PolixiaZuulGatewayMain.class);
+    public static void main(String[] args) {
+        SpringApplication.run(PolixiaZuulGatewayMain.class, args);
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+    }
+}
